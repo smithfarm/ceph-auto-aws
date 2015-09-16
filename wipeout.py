b213ef8c06441d7088d3082abcd47b5a953aefd2
Nathan Cutler <ncutler@suse.cz>
2015-09-16 12:04:11 +0200

    wipeout.py: finish basic script
    Signed-off-by: Nathan Cutler <ncutler@suse.cz>

--- wipeout.py
+++ wipeout.py
@@ -54,8 +54,42 @@ g['vpc_obj'] = init_lib.init_vpc( g['vpc_conn'], n['cidr-block'] )
 delegate = int(args.delegate)
 cidr_block = '10.0.{}.0/24'.format(delegate)
 
-#PC * Does that subnet even exist?
+#PC * Get subnet object (raise exception if it doesn't exist).
 g['subnet_obj'] = init_lib.init_subnet( g['vpc_conn'], cidr_block )
-print g['subnet_obj']
-
 print "Wiping out all instances in {} (and attached volumes)".format(cidr_block)
+
+#PC * Get all instances in the subnet.
+g['instances'] = g['ec2_conn'].get_only_instances( 
+    filters={ "subnet-id": g['subnet_obj'].id } 
+)
+
+#PC * Loop over the instances and get their OSD volumes ('/dev/sdb').
+g['volumes'] = []
+for i in g['instances']:
+    vols = g['ec2_conn'].get_all_volumes(
+        filters={ "attachment.instance-id": i.id, "attachment.device": "/dev/sdb" }
+    )
+    for v in vols:
+        g['volumes'].append((v, i))
+        g['ec2_conn'].detach_volume( v.id, instance_id=i.id )
+
+print "Wiping out {} instances and {} attached OSD volumes".format(
+    len(g['instances']),
+    len(g['volumes'])
+)
+
+for mapping in g['volumes']:
+    v = mapping[0]
+    i = mapping[1]
+    init_lib.wait_for_detachment( g['ec2_conn'], v.id, i.id )            
+
+for v in g['volumes']:
+    g['ec2_conn'].delete_volume( v.id )
+
+id_list = []
+for i in g['instances']:
+    id_list.append( i.id )
+if id_list:
+    g['ec2_conn'].terminate_instances( instance_ids=id_list )
+
+print "Done."
