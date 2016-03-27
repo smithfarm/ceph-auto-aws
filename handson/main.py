eca52b56d9230810cba0c0e98220de0ca35e8779
Nathan Cutler <ncutler@suse.com>
2016-03-28 01:49:48 +0200

    Bump version: 0.0.15 → 0.0.16

--- handson/main.py
+++ handson/main.py
@@ -40,7 +40,7 @@ from handson.probe import ProbeYaml
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s')
 
-__version__ = "0.0.15"
+__version__ = "0.0.16"
 
 
 class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
