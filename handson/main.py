d973de519798bcd7c149a97be7427cdb9242a752
Nathan Cutler <ncutler@suse.com>
2016-04-06 22:38:59 +0200

    Bump version: 0.1.4 → 0.1.5

--- handson/main.py
+++ handson/main.py
@@ -45,7 +45,7 @@ from handson.wipeout import WipeOut
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s')
 log = logging.getLogger(__name__)
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 
 class HandsOn(object):
