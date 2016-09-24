4bb44adbfee25cdb9ddc206952383e6153463704
Karol Mroz <kmroz@suse.de>
2016-09-23 18:38:23 -0700

    generate-keys: grab number of delegates from aws yaml
    Signed-off-by: Karol Mroz <kmroz@suse.de>

--- generate-keys.sh
+++ generate-keys.sh
@@ -2,8 +2,12 @@ if [ -d keys ] ; then
     echo "keys/ directory already exists - bailing out!"
     exit 1
 fi
+if [ ! -e "./aws.yaml" ] ; then
+    echo "./aws.yaml does not exist. Run `ho probe yaml` to create. - bailing out!"
+    exit 1
+fi
 mkdir keys
-DELEGATES=50
+DELEGATES="$(grep "delegates:" ./aws.yaml | awk '{ print $2 }')"
 for d in $(seq 0 $DELEGATES) ; do
     ssh-keygen -b 1024 -t rsa -f "keys/$(whoami)-d$d" -q -N "" -C "$(whoami)-d$d.localhost"
 done
