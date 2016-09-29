91f6b7675d81cf74a3c927a8f871a592610b7523
Nathan Cutler <ncutler@suse.com>
2016-09-29 13:04:53 +0200

    scripts: more error-checking in generate-keys.sh
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- generate-keys.sh
+++ generate-keys.sh
@@ -1,14 +1,28 @@
-if [ -d keys ] ; then
-    echo "keys/ directory already exists - bailing out!"
-    exit 1
-fi
+#!/bin/bash
+
 if [ ! -e "./aws.yaml" ] ; then
     echo "./aws.yaml does not exist. Run `ho probe yaml` to create. - bailing out!"
     exit 1
 fi
-mkdir keys
+
 DELEGATES="$(grep "delegates:" ./aws.yaml | awk '{ print $2 }')"
+if [ "$DELEGATES" -ne "$DELEGATES" ] 2>/dev/null ; then
+    echo "Could not obtain number of delegates from YAML."
+    exit 1
+fi
+if [ "$DELEGATES" -gt 100 ] || [ "$DELEGATES" -lt 1 ] ; then
+    echo "Number of delegates (in YAML) must be between 1 and 100."
+    exit 1
+fi
+
+if [ -d keys ] ; then
+    echo "keys/ directory already exists - bailing out!"
+    exit 1
+fi
+mkdir keys
+
 for d in $(seq 0 $DELEGATES) ; do
     ssh-keygen -b 1024 -t rsa -f "keys/$(whoami)-d$d" -q -N "" -C "$(whoami)-d$d.localhost"
 done
+
 chmod 400 keys/$(whoami)*
