7a2fe95b9f50e735f3f877c03ff79bd366614f52
Nathan Cutler <ncutler@suse.com>
2017-10-16 11:33:52 +0200

    deepsea-install.sh: install DeepSea from filesystems:ceph
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- states/deepsea-install.sh
+++ states/deepsea-install.sh
@@ -3,20 +3,15 @@
 # Script used by deepsea-salt-master.sls to install DeepSea on Delegate (local)
 # master node
 
-#---------------------------------------------------------
-#--- can be dropped once python-click reaches SES5 channel
-#---------------------------------------------------------
-sudo zypper --non-interactive --no-gpg-checks addrepo http://download.opensuse.org/repositories/devel:/languages:/python/SLE_12_SP3/devel:languages:python.repo
+# install DeepSea from RPM
+sudo zypper --non-interactive --no-gpg-checks addrepo http://download.opensuse.org/repositories/filesystems:ceph/SLE_12_SP3/filesystems:ceph.repo
 sudo zypper --non-interactive --no-gpg-checks refresh
-sudo zypper --non-interactive --no-gpg-checks install --no-recommends python-click
-sudo zypper --non-interactive --no-gpg-checks removerepo devel_languages_python
-#---------------------------------------------------------
-#--- can be dropped once python-click reaches SES5 channel
-#---------------------------------------------------------
+sudo zypper --non-interactive --no-gpg-checks install --no-recommends deepsea
 
-sudo zypper --non-interactive --no-gpg-checks install --no-recommends make rpm
-test -d DeepSea || git clone --depth 1 --branch susecon2017 https://github.com/smithfarm/DeepSea.git
-cd DeepSea
-ls -l
-sudo make install
-sudo zypper --non-interactive install --no-recommends $(rpmspec --requires -q -v deepsea.spec | grep manual | awk '{print $2}')
+# install DeepSea from source
+#sudo zypper --non-interactive --no-gpg-checks install --no-recommends make rpm
+#test -d DeepSea || git clone --depth 1 --branch susecon2017 https://github.com/smithfarm/DeepSea.git
+#cd DeepSea
+#ls -l
+#sudo make install
+#sudo zypper --non-interactive install --no-recommends $(rpmspec --requires -q -v deepsea.spec | grep manual | awk '{print $2}')
